640b8ba8ab5ae1f31c8dee4ac78f43fa566e500f
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2017-12-02 15:09:46 +0100

    added migration for product data fields

---
+++ db/migrate/20171202135853_add_data_to_product.rb
@@ -0,0 +1,11 @@
+class AddDataToProduct < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :barcode, :string
+    add_column :products, :best_before, :date
+    add_column :products, :description, :text
+    add_column :products, :fridge_temp, :integer
+    add_column :products, :recommendations, :text
+    add_column :products, :plus_days_min, :integer
+    add_column :products, :plus_days_max, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202133838) do
+ActiveRecord::Schema.define(version: 20171202135853) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "barcode"
+    t.date "best_before"
+    t.text "description"
+    t.integer "fridge_temp"
+    t.text "recommendations"
+    t.integer "plus_days_min"
+    t.integer "plus_days_max"
   end
 
 end
